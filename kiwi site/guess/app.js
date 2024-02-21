e94dd7f621869136cd5009583286e5f9ac389af6
Butch-Smith <157831810+kiwiflavoredstar@users.noreply.github.com>
2024-02-21 19:48:32 +0100

    added a random combination to guess!! and moved some things around in the code so that it's a bit more organised!!

--- kiwi site/guess/app.js
+++ kiwi site/guess/app.js
@@ -1,3 +1,4 @@
+//all of these variables define the number slots and the sounds used!!
 const slotOne = document.getElementById('slotOne');
 const slotTwo = document.getElementById('slotTwo');
 const slotThree = document.getElementById('slotThree');
@@ -5,11 +6,22 @@ const slotFour = document.getElementById('slotFour');
 const audioWrong = new Audio('audio/extremely-loud-incorrect-buzzer-made-with-Voicemod.mp3');
 const audioCorrect = new Audio('audio/extremely-loud-correct-buzzer.mp3');
 
+//these variables make it so that the numbers in the slots start at zero!!
 let one = 0;
 let two = 0;
 let three = 0;
 let four = 0;
 
+//there variables generates the numbers for the random combination!!
+let randomNumberOne = Math.floor(Math.random() * 10);
+let randomNumberTwo = Math.floor(Math.random() * 10);
+let randomNumberThree = Math.floor(Math.random() * 10);
+let randomNumberFour = Math.floor(Math.random() * 10);
+
+//this beuutiful line here shows the correct random combination in the console!!
+console.log(randomNumberOne, randomNumberTwo, randomNumberThree, randomNumberFour);
+
+//these four functions make it so that the numbers in the slots increase when clicked and reset when they go past nine!!
 function increaseOne() {
     if (slotOne.innerHTML < 9) {
         one++
@@ -51,7 +63,12 @@ function increaseFour() {
     }
 }
 
+//this function checks if the combination is correct or incorrect!!
 function check() {
+    if (one == randomNumberOne && two == randomNumberTwo && three == randomNumberThree && four == randomNumberFour) {
+        audioCorrect.play();
+        alert("correct!!")
+    }
     if (one == 2 && two == 9 && three == 5 && four == 9) {
         audioCorrect.play();
         alert("correct!!")
